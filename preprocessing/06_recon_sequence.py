c89a65d2eef326380c8c148f1655b35cfef9b19a
RaphaelSura <raphaeldaveau@protonmail.com>
2022-02-25 10:21:23 +0100

    added multiprocessing

--- preprocessing/06_recon_sequence.py
+++ preprocessing/06_recon_sequence.py
@@ -4,13 +4,14 @@ import sys
 import subprocess
 import shutil
 import time
-from tqdm import tqdm
-import json
+import multiprocessing as mp
+# from tqdm import tqdm
+# import json
 import argparse
 from pathlib import Path
 import pandas as pd
 import yaml
-# from multiprocessing import Pool
+from itertools import repeat
 
 
 def edit_params_file(base_file, temp_file, formatted_dur):
@@ -32,7 +33,7 @@ def edit_params_file(base_file, temp_file, formatted_dur):
         f.write(new_params)
 
 
-def e7_sequence(input_dir, output_dir, param_file, recon_type, is_test):
+def e7_sequence(input_dir, output_dir, param_file, recon_type):
 
     # setting up the directory names
     data_dir_converted = input_dir.parent.joinpath(
@@ -73,19 +74,6 @@ def e7_sequence(input_dir, output_dir, param_file, recon_type, is_test):
         hdr_file_naming += "_000"
         hdr_file = f"{input_dir.name}{hdr_file_naming}.v.hdr"
 
-    # check inside the header file for pixel spacing in test mode
-    if is_test:
-        with open(hdr_file) as hf:
-            hf_content = hf.readlines()
-        hf_content = [
-            l for l in hf_content if 'scale factor (mm/pixel) [1]' in l
-        ]
-        print(*hf_content)
-        print(
-            'Remember to check zoom value in param file to calculate accordingly!'
-        )
-        sys.exit(-1)
-
     # running IF2Dicom.js
     subprocess.run([
         "cscript", "C:\\JSRecon12\\IF2Dicom.js", hdr_file,
@@ -106,7 +94,71 @@ def e7_sequence(input_dir, output_dir, param_file, recon_type, is_test):
     # go back and delete converted data
     os.chdir("..\\..")
     time.sleep(2)
+    if data_dir_converted.exists():
+        shutil.rmtree(data_dir_converted, ignore_errors=True)
     print('Deleting:', data_dir_converted)
+
+
+def process_wrapper(patient_folder, configs, df, out_dir):
+
+    mode = configs['activity']
+    acq_time = configs['acq_time']
+    delay_time = configs['start_delay_for_recon']
+    percent = configs['percent']
+    blurring = configs['blurring']
+    recon_type = configs['recon_type']
+    project_id = configs['project']  # also base folder name
+    # DRIVE letter on WINDOWS
+    drive = Path(patient_folder.drive + '/')
+    e7_params_file_path = drive.joinpath(configs['e7_params_file_path'])
+    # get the parameter file from arg or automatically
+    e7_params = e7_params_file_path.joinpath(
+        f"{project_id}_jsrecon_params_{blurring}mm.txt")
+    print(e7_params)
+    if not e7_params.exists():
+        raise FileNotFoundError(
+            """ Could not find params file for JSRecon to run. Check config file."""
+        )
+
+    # create temp folder to store individual params file
+    temp_folder = e7_params_file_path.joinpath("temp")
+    temp_folder.mkdir(parents=True, exist_ok=True)
+
+    patient_id = patient_folder.name
+    # else edit params file and do reconstruction
+    delay_for_recon = df.loc[patient_id, 'delay for recon']
+
+    data_folder = patient_folder.joinpath('PET')
+    output_folder = f"PET_{blurring}mm"
+    if mode == 'dose reduction':
+        data_folder = patient_folder.joinpath(f"PET_LD{percent}pct")
+        output_folder = f"PET_{blurring}mm_LD{percent}pct"
+
+    ####### special for time reduction recons ###########
+    # pib 540:120 (9min:2min)    570:60  (1min)
+    # pib 600:300 (10min:5min)
+    # pe2i 270:60 (4min30sec:1min)     285:30 (0.5 min)
+    elif mode == 'time reduction':
+        if delay_for_recon != 'all':
+            delay_time = int(delay_for_recon.split(':')[0]) + delay_time
+        delay_for_recon = f'{delay_time}:{acq_time}'
+
+        output_folder = f"PET_{blurring}mm_LD{acq_time}sec{delay_time}delay"
+    ###########################################
+
+    output_folder = out_dir.joinpath(patient_id).joinpath(output_folder)
+    # create temporary params file
+    temp_params_file = temp_folder.joinpath(
+        f"{patient_id}_{output_folder.name}_{e7_params.stem}")
+    edit_params_file(e7_params, temp_params_file, delay_for_recon)
+
+    try:
+        e7_sequence(data_folder, output_folder, temp_params_file, recon_type)
+    except Exception as e:
+        print(e)
+
+    # remove temp file
+    temp_params_file.unlink()
 
 
 def main():
@@ -130,56 +182,42 @@ def main():
                         type=str,
                         default=Path(
                             os.getcwd()).parent.joinpath('recon_config.yaml'))
-    parser.add_argument("-t",
-                        "--test",
-                        help="Test run for 1 patient",
-                        action="store_true",
-                        default=False)
+    parser.add_argument("-mp",
+                        "--multiprocess",
+                        help="How many processes to use in parallel.",
+                        type=int,
+                        default=0)
     args = parser.parse_args()
 
     data_dir = Path(args.input)
 
     with open(args.config) as cf:
-        config = yaml.load(cf, Loader=yaml.FullLoader)
+        config = yaml.safe_load(cf)
 
     recon_data_dir = data_dir.parent.joinpath(args.output)
     recon_data_dir.mkdir(parents=True, exist_ok=True)
-    base_folder = Path(data_dir.drive + '/')
-    project_id = config['project']
-    recon_type = config['recon_type']
+
     lower_thresh = config['dose_threshold']
     scanner_name = config['scanner']
-    blurring = config['blurring']
-    e7_params_file_path = Path(config['e7_params_file_path'])
-    mode = config['activity']
-    acq_time = config['acq_time']
-    delay_time = config['start_delay_for_recon']
-    percent = config['percent']
 
-    is_test = args.test
-    if is_test:
-        print('This is a test run on 1 patient..')
-
-    lm_info_gen = data_dir.glob(f'*anonymized_patient_info.csv')
-    lm_info = [f for f in lm_info_gen if f.parent.name in data_dir.name]
+    pt_file_info_gen = data_dir.glob(f'*anonymized_patient_info.csv')
+    pt_file_info = [
+        f for f in pt_file_info_gen if f.parent.name in data_dir.name
+    ]
     # lm_info = next(lm_info_gen)
-    lm_info = lm_info[0]
-    df = pd.read_csv(lm_info, index_col=0)
-    # make copy of df into output dir (recon)
-    # df.to_csv(recon_data_dir.joinpath(lm_info.name))
+    pt_file_info = pt_file_info[0]
+    df = pd.read_csv(pt_file_info, index_col=0)
 
-    # get the parameter file from arg or automatically
-    e7_params = e7_params_file_path.joinpath(
-        f"{project_id}_jsrecon_params_{blurring}mm.txt")
+    print(
+        "\n################ RECONSTRUCTION STARTING WITH CONFIG ##############"
+    )
+    for k, v in config.items():
+        print(k.ljust(40), v)
+    print(
+        "#####################################################################\n"
+    )
 
-    if not e7_params.exists():
-        raise FileNotFoundError(
-            """ Could not find params file for JSRecon to run. Check config file."""
-        )
-
-    print(f"RECONSTRUCTION STARTING with recon type {recon_type}")
-
-    # make filters on df and use index for for-loop
+    # filter which patients to reconstruct - based on config file
     df_filter = df.copy()
     # filtering based on scanner type
     if scanner_name != 'ALL':
@@ -196,74 +234,23 @@ def main():
     patient_folders = [
         d for d in data_dir.iterdir() if d.is_dir() if d.name in good_patients
     ]
-    if is_test:
-        patient_folders = patient_folders[:1]
 
-    # create temp folder to store individual params file
-    temp_folder = e7_params.parent.joinpath("temp")
-    temp_folder.mkdir(parents=True, exist_ok=True)
+    # parallelize patient reconstruction
+    if args.multiprocess:
+        with mp.Pool(args.multiprocess) as p:
+            # need to use repeat + zip as the patient_folders is a list but the
+            # other args are unique values.
+            p.starmap(
+                process_wrapper,
+                zip(patient_folders, repeat(config), repeat(df),
+                    repeat(recon_data_dir)))
 
-    fails = {}
-    for pf in tqdm(patient_folders):
-
-        patient_id = pf.name
-        # else edit params file and do reconstruction
-        delay_for_recon = df.loc[patient_id, 'delay for recon']
-
-        data_folder = pf.joinpath('PET')
-        output_folder = f"PET_{blurring}mm"
-        if mode == 'dose reduction':
-            data_folder = pf.joinpath(f"PET_LD{percent}pct")
-            output_folder = f"PET_{blurring}mm_LD{percent}pct"
-        ####### special for time reduction recons ###########
-        # pib 540:120 (9min:2min)    570:60  (1min)
-        # pib 600:300 (10min:5min)
-        # pe2i 270:60 (4min30sec:1min)     285:30 (0.5 min)
-        elif mode == 'time reduction':
-            if delay_for_recon != 'all':
-                delay_time = int(delay_for_recon.split(':')[0]) + delay_time
-            delay_for_recon = f'{delay_time}:{acq_time}'
-
-            output_folder = f"PET_{blurring}mm_LD{acq_time}sec{delay_time}delay"
-        ###########################################
-        output_folder = recon_data_dir.joinpath(patient_id).joinpath(
-            output_folder)
-        # create temporary params file
-        temp_params_file = temp_folder.joinpath(
-            f"{patient_id}_{output_folder.name}_{e7_params.stem}")
-        edit_params_file(e7_params, temp_params_file, delay_for_recon)
-
-        try:
-            e7_sequence(data_folder, output_folder, temp_params_file,
-                        recon_type, is_test)
-        except Exception as e:
-            print(e)
-            fails[patient_id] = str(e)
-
-        # remove temp file
-        temp_params_file.unlink()
-
-    if fails:
-        print(f'\n\nFailed to reconstruct {len(fails)} datasets')
-        json_file = data_dir.joinpath(
-            f'{project_id}_failed_reconstructions_with_code.json')
-        with open(json_file, 'w') as outfile:
-            json.dump(fails, outfile, indent=1)
+    else:
+        for patient in patient_folders:
+            process_wrapper(patient)
 
     print("RECONSTRUCTION DONE")
     print("JOB COMPLETE!\n")
-    """ in principle we can multiprocess this script, 
-        however there's a Windows related error on the recon machines
-        
-        Would simply need to put what's inside the for loop in a function:
-        
-        for pf in tqdm(patient_folders): --> def process_wrapper(pf):
-            ...
-        
-        with Pool(20) as p:
-            p.map(process_wrapper, patient_folders)
-        
-        """
 
 
 if __name__ == '__main__':
